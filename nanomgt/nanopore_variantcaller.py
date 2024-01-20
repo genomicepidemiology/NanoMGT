7349b44a8bbbc24e8ea21a9f22149c4e22874552
MBHallgren <s153002@student.dtu.dk>
2024-01-21 08:22:54 +1100

    1.0.1

--- nanomgt/nanopore_variantcaller.py
+++ nanomgt/nanopore_variantcaller.py
@@ -101,7 +101,6 @@ def nanopore_metagenomics_variantcaller(arguments):
 
     sys.exit()
 
-
 def highest_scoring_hit(file_path):
     """
     Identifies and returns the highest scoring template from a tab-separated file.
