6eaa5fe99457f55b28d5aff25a6c93928028401a
MBHallgren <s153002@student.dtu.dk>
2024-04-05 14:38:05 +1100

    consensus test5

--- nanomgt/nanopore_mutations.py
+++ nanomgt/nanopore_mutations.py
@@ -1,4 +1,29 @@
 import re
+from Bio import pairwise2
+from Bio.Seq import Seq
+
+def align_and_identify_mutations(seq1, seq2):
+    # seq1 = qeury, seq2 = ref
+    match_score = 2  # Positive score for matches, encourages alignment to match characters
+    mismatch_score = -3  # Negative score for mismatches, penalizes alignment for mismatching characters
+    gap_open = -5  # Negative score for opening a gap, makes the algorithm less willing to introduce gaps
+    gap_extend = -2  # Negative score for extending a gap, penalizes extending gaps over single mismatches
+    # Perform local alignment with custom scoring
+    alignments = pairwise2.align.localms(seq1, seq2, match_score, mismatch_score, gap_open, gap_extend)
+
+    # Assuming we take the first alignment (typically the highest scoring)
+    alignment_a, alignment_b, score, begin, end = alignments[0]
+    print(alignment_a)
+    print(alignment_b)
+
+    # Identify point mutations, excluding gaps
+    point_mutations = []
+    for i in range(len(alignment_a)):
+        if alignment_a[i] != alignment_b[i]:
+            point_mutations.append((i + 1, alignment_a[i], alignment_b[i]))  # i+1 for 1-based indexing
+
+    return point_mutations
+
 
 def extract_alignment(reference, query, cigar_string):
     """
@@ -103,6 +128,7 @@ def identify_mutations(mutation_vector, reference_sequence, gene_mutations, read
         print (len(mutation_vector), len(reference_sequence))
         print (mutation_vector)
         print (reference_sequence)
+        point_mutations = align_and_identify_mutations(reference_sequence, ''.join(mutation_vector))
         raise ValueError("The mutation vector and reference sequence must have the same length.")
 
     for i in range(len(mutation_vector)):
