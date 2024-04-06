945fac26a5c68b10dacc30b24e126724bc742b9c
MBHallgren <s153002@student.dtu.dk>
2024-04-06 19:33:45 +1100

    push6

--- nanomgt/nanopore_variantcaller.py
+++ nanomgt/nanopore_variantcaller.py
@@ -624,11 +624,6 @@ def upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, con
     for allele in confirmed_mutation_dict:
         mutation_list = confirmed_mutation_dict[allele][0]
         num_mutations = len(mutation_list)
-        print (allele, 'ALLELE')
-        print (mutation_list, 'MUTATION LIST')
-        sys.exit()
-        #if allele == 'BACT000001_1153':
-        #    print (mutation_list, 'MUTATION LIST')
         if num_mutations > 1:
             co_occurrence_matrix = [[0] * num_mutations for _ in range(num_mutations)]
             for read in reads_mutation_dict:
@@ -636,6 +631,8 @@ def upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, con
                 if read_allele == allele:
                     read_mutations = reads_mutation_dict[read]
                     valid_mutations = [mutation for mutation in read_mutations if mutation in mutation_list]
+                    if allele == 'BACT000001_1153':
+                        print (read, valid_mutations)
                     if len(valid_mutations) > 1:
                         for i in range(len(valid_mutations)):
                             for j in range(i + 1, len(valid_mutations)):
