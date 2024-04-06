f0c2d530e0725f04a49d2209b7972c6c9d12fb85
MBHallgren <s153002@student.dtu.dk>
2024-04-06 20:41:09 +1100

    push23

--- nanomgt/nanopore_variantcaller.py
+++ nanomgt/nanopore_variantcaller.py
@@ -632,10 +632,11 @@ def upper_co_occuring_mutations_in_reads(arguments, confirmed_mutation_dict, con
                     read_mutations = reads_mutation_dict[read]
                     valid_mutations = [mutation for mutation in read_mutations if mutation in mutation_list]
                     if allele == 'BACT000001_1153':
-                        print (read)
-                        print (valid_mutations)
-                        print (read_mutations)
-                        print (mutation_list)
+                        if read == 'SRR27755678.258255':
+                            print (read)
+                            print (valid_mutations)
+                            print (read_mutations)
+                            print (mutation_list)
                     if len(valid_mutations) > 1:
                         for i in range(len(valid_mutations)):
                             for j in range(i + 1, len(valid_mutations)):
