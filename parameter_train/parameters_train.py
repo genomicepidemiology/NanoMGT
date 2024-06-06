708f476747d2cadc62c8d608bf15d23bec684d70
MBHallgren <s153002@student.dtu.dk>
2024-06-06 12:52:46 +0200

    train 41

--- parameter_train/parameters_train.py
+++ parameter_train/parameters_train.py
@@ -75,7 +75,7 @@ def train_parameters(maf, results_folder, min_n, cor, new_output_folder,
 
 
     # Format and output the results
-    nvc.format_output(new_output_folder, confirmed_mutation_dict, consensus_dict, bio_validation_dict,
+    format_output(new_output_folder, confirmed_mutation_dict, consensus_dict, bio_validation_dict,
                   co_occurrence_tmp_dict)
 
     #sample = results_folder.split('/')[-1]
@@ -100,6 +100,46 @@ def train_parameters(maf, results_folder, min_n, cor, new_output_folder,
 
 
     return f1, parameter_string, precision, recall, tp, fp, fn
+
+def format_output(new_output_folder, confirmed_mutation_dict, consensus_dict, bio_validation_dict, co_occurrence_tmp_dict):
+    """
+    Format and print the output of confirmed mutations with additional information.
+
+    Args:
+        confirmed_mutation_dict (dict): A dictionary containing confirmed mutations for alleles.
+        consensus_dict (dict): A dictionary containing consensus information for alleles.
+        bio_validation_dict (dict): A dictionary containing biological validation data for genes.
+
+    Returns:
+        None
+    """
+    with open(new_output_folder + '/minor_mutations.csv', 'w') as outfile:
+        header = 'Gene,Position,MajorityBase,MutationBase,MutationDepth,TotalDepth,GeneLength,MutationComment,CoOccurrence'
+        print(header, file=outfile)
+
+        for allele in confirmed_mutation_dict:
+            for mutation in zip(confirmed_mutation_dict[allele][0], confirmed_mutation_dict[allele][1]):
+                position = mutation[0].split('_')[0]
+                mutation_base = mutation[0].split('_')[1]
+                mutation_depth = mutation[1]
+                majority_base = consensus_dict[allele][1][int(position) - 1]
+                total_depth = sum(consensus_dict[allele][0][int(position) - 1])
+                biological_existence = check_single_mutation_existence(bio_validation_dict, allele, mutation[0])
+                gene_length = len(consensus_dict[allele][1])
+                if mutation[0] in co_occurrence_tmp_dict[allele]:
+                    co_occurrence = 'Yes'
+                else:
+                    co_occurrence = 'No'
+
+                if biological_existence:
+                    print('{},{},{},{},{},{},{},{},{}'.format(allele, position, majority_base, mutation_base,
+                                                              mutation_depth, total_depth, gene_length,
+                                                              'Mutation seen in database', co_occurrence), file=outfile)
+                else:
+                    print('{},{},{},{},{},{},{},{},{}'.format(allele, position, majority_base, mutation_base,
+                                                              mutation_depth, total_depth, gene_length,
+                                                              'Novel mutation', co_occurrence), file=outfile)
+
 
 def run_jobs_in_parallel(max_workers, new_output_folder, alignment_folder, maf, parameters):
     # Fixed parameters
