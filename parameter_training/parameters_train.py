951fd2ac275b7e35b0b9fed5b9b07b360868a0f5
MBHallgren <s153002@student.dtu.dk>
2024-06-13 14:07:13 +0200

    test33

--- parameter_training/parameters_train.py
+++ parameter_training/parameters_train.py
@@ -562,6 +562,7 @@ for param in param_list:
         total_parameter_results[param][maf] = {}
         print (os.path.join(output_training_folder, "{}".format('maf_' + str(maf))))
         for folder in os.path.join(output_training_folder, "{}".format('maf_' + str(maf))):
+            print (folder)
             if folder.startswith(param):
                 batch_id = int(folder.split('_')[-2][5:])
                 results_file = os.path.join(output_training_folder, "{}".format('maf_' + str(maf)), folder, 'all_results.csv')
