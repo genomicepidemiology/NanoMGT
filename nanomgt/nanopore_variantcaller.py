6595a244f7f6d444be1658be357f770659aa738d
MBHallgren <s153002@student.dtu.dk>
2024-06-14 12:25:22 +0200

    test95

--- nanomgt/nanopore_variantcaller.py
+++ nanomgt/nanopore_variantcaller.py
@@ -650,8 +650,6 @@ def derive_mutation_positions(consensus_dict, min_n, maf, cor):
     all_confirmed_mutation_dict = {}
 
     for allele, allele_data in consensus_dict.items():
-        print (allele)
-        print (allele_data)
         all_confirmed_mutation_dict[allele] = [[], []]
 
         for i in range(len(allele_data[0])):
