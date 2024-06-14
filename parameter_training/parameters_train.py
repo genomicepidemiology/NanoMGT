715efc5466e6adce09492476d054d770935d11c2
MBHallgren <s153002@student.dtu.dk>
2024-06-14 10:24:56 +0200

    test62

--- parameter_training/parameters_train.py
+++ parameter_training/parameters_train.py
@@ -29,11 +29,12 @@ folders = [f for f in os.listdir(alignment_results_path)]
 
 maf_interval = [5]
 
-cor_interval_search = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+#Consider how many parameters you want you search here depending on runtime.
+cor_interval_search = [0.2, 0.3, 0.4, 0.5, 0.6]
 dp_interval_search = [0.1, 0.2, 0.3, 0.4, 0.5]
 np_interval_search = [1, 1.5, 2, 2.5, 3, 3.5]
 pp_interval_search = [0.1, 0.,2, 0.3, 0.4, 0.5]
-ii_interval_search = [0.01, 0.1, 0.2, 0.3, 0.40]
+ii_interval_search = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5]
 
 parameters_interval_search = {
     'cor_interval': cor_interval_search,
