b3238f26d94f00b37a0cb3564ba3144fd09219f9
MBHallgren <s153002@student.dtu.dk>
2024-06-17 12:33:19 +0200

    17_test_30

--- parameter_training/parameters_train.py
+++ parameter_training/parameters_train.py
@@ -347,7 +347,7 @@ def determine_gradient_value(df, param):
         f1_scores_normalized = f1_scores_new - f1_scores_first_value
         spline = UnivariateSpline(param_values_new, f1_scores_normalized, s=None)
         derivative = spline.derivative()
-        param_dense_normalized = np.linspace(0, 1, 450)
+        param_dense_normalized = np.linspace(min(param_values_new), max(param_values_new), 450)
         print (param_dense_normalized)
         f1_dense_normalized = spline(param_dense_normalized)
         print (f1_dense_normalized)
