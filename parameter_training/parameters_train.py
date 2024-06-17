5c26bdfa19b883dde8b934071b07f3a76c13b919
MBHallgren <s153002@student.dtu.dk>
2024-06-17 11:12:15 +0200

    17_test_5

--- parameter_training/parameters_train.py
+++ parameter_training/parameters_train.py
@@ -335,7 +335,7 @@ def determine_gradient_value(df, param):
         param_values_normalized = (param_values - np.min(param_values)) / param_values_range
         f1_scores_normalized = (f1_scores - np.min(f1_scores)) / f1_scores_range
         print ('normalized')
-        print (param_values_normalized)
+        print (param_values)
         print (f1_scores_normalized)
         sys.exit()
         sorted_indices = np.argsort(param_values_normalized)
