0352fcaa0b49d7bfd32fff7b1988114911970dfd
MBHallgren <s153002@student.dtu.dk>
2024-06-23 20:35:11 +0200

    test2

--- parameter_training/parameters_train.py
+++ parameter_training/parameters_train.py
@@ -185,7 +185,7 @@ def run_jobs_in_parallel(max_workers, new_output_folder, alignment_folder, maf, 
                                               os.path.join(alignment_folder, 'rmlst_alignment.mat'))
     bio_validation_dict = nvc.bio_validation_mutations(consensus_dict, os.path.join(alignment_folder, 'specie.fsa'))
 
-
+    print (alignment_folder)
     sample = alignment_folder.split('/')[-1]
     print (sample)
     sys.exit()
