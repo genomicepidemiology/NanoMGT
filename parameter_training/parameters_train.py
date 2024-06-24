645e974035521ecd2f13f39776cd92e74bcc8a45
MBHallgren <s153002@student.dtu.dk>
2024-06-24 12:02:11 +0200

    test6

--- parameter_training/parameters_train.py
+++ parameter_training/parameters_train.py
@@ -33,7 +33,7 @@ cor_interval_search = [0.1, 0.3, 0.5]
 dp_interval_search = [0.1, 0.2, 0.3]
 np_interval_search = [1, 2, 3]
 pp_interval_search = [0.1, 0.2, 0.3]
-ii_interval_search = [0.1, 0.2, 0.3
+ii_interval_search = [0.1, 0.2, 0.3]
 
 
 parameters_interval_search = {
