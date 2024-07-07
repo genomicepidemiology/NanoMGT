d3586fc9aed342e1fa757ad22fb26d390a1a7d3f
MBHallgren <s153002@student.dtu.dk>
2024-07-07 08:29:49 +0200

    benchmarking parameters for mixed_ run clean data set2

--- parameter_training/benchmarck_nanomgt_alignments_mixed.py
+++ parameter_training/benchmarck_nanomgt_alignments_mixed.py
@@ -18,11 +18,11 @@ from nanomgt import nanopore_variantcaller as nvc
 #NOTE: This is for benchmarking mixed' parameters for the 3 data sets.
 
 # Modify these paths as needed.
-alignment_results_path = '/home/projects/cge/people/malhal/nanomgt_new_results/clean_validation/'
+alignment_results_path = '/home/projects/cge/people/malhal/nanomgt_new_results/contaminated_validation/'
 maps_path = '/home/projects/cge/people/malhal/nanomgt_reads/variant_maps/'
-json_info_path = '/home/projects/cge/people/malhal/nanomgt_json/simulated_batches_clean'
+json_info_path = '/home/projects/cge/people/malhal/nanomgt_json/simulated_batches_contaminated'
 training_or_validation_extension_json = '_validation.json'
-output_training_folder = 'clean_benchmark'
+output_training_folder = 'contaminated_benchmark'
 param_list = ['np', 'cor', 'pp', 'dp', 'ii']
 maf_interval = [5, 4, 3, 2, 1]
 cpus = 35
