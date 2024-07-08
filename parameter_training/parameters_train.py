53e825ff699ed9dd956ddfab92a18fd137be19e0
MBHallgren <s153002@student.dtu.dk>
2024-07-08 22:18:58 +0200

    test training 9

--- parameter_training/parameters_train.py
+++ parameter_training/parameters_train.py
@@ -519,7 +519,7 @@ def run_round_of_parameter_search(round_number, maf_interval, folders, output_fo
                 batch_id -= 10
             if batch_id >= maf:
                 alignment_folder = os.path.join(alignment_results_path, folder)
-                path = os.path.join(output_folder, maf, folder)
+                path = os.path.join(output_folder, folder, 'maf_' + str(maf))
                 new_output_folder = setup_directory(path)
                 run_jobs_in_parallel(cpus, new_output_folder, alignment_folder, maf / 100,
                                      parameters_interval_search, maps_path, json_info_path,
